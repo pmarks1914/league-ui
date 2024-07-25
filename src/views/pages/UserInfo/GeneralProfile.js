0ba505987b65cec2f474a76233c1eea1d8eab9f9
Patrick <pmarks1914@gmail.com>
2024-07-25 11:15:51 +0000

    tab manage

--- src/views/pages/UserInfo/GeneralProfile.js
+++ src/views/pages/UserInfo/GeneralProfile.js
@@ -22,34 +22,6 @@ import { getSessionTimeout } from '../../../Utils/Utils';
 import BasicInfo from './Profile/BasicInfo';
 import College from './College.js/College';
 
-let date = new Date();
-// let dateYearInterval = 100;
-// let i = 1;s
-
-// const createOption = (label, dataId) => ({
-//   label,
-//   value: dataId,
-// });
-let optionsFinYear = [...Array(100)].map((x, i) => {
-  return ({ value: (date.getUTCFullYear() - 100 + 1 + i), label: (date.getUTCFullYear() - 100 + 1 + i), key: i + 1 })
-  // return createOption( (date.getUTCFullYear()-100+1+i), (date.getUTCFullYear()-100+1+i) )
-}
-);
-const optionsFinMonth = [
-  // {value: "", label: "Select Fin-Month", icon: "", isDisabled: true },
-  { value: "January", label: "January", key: 1 },
-  { value: "February", label: "February", key: 2 },
-  { value: "March", label: "March", key: 3 },
-  { value: "April", label: "April", key: 4 },
-  { value: "May", label: "May", key: 5 },
-  { value: "June", label: "June", key: 6 },
-  { value: "July", label: "July", key: 7 },
-  { value: "August", label: "August", key: 8 },
-  { value: "September", label: "September", key: 9 },
-  { value: "October", label: "October", key: 10 },
-  { value: "November", label: "November", key: 11 },
-  { value: "December", label: "December", key: 12 },
-];
 
 const General = () => {
   const [activeTab, setActiveTab] = useState('1');
@@ -96,7 +68,15 @@ const General = () => {
             className={classnames({ active: activeTab === '1' })}
             onClick={() => { toggle('1'); }}
           >
-            <h6>Profile</h6>
+            <h6>Basic Information</h6>
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '2' })}
+            onClick={() => { toggle('2'); }}
+          >
+            <h6>Education Information</h6>
           </NavLink>
         </NavItem>
         <br /><br />
@@ -108,7 +88,7 @@ const General = () => {
                 <Nav className="mt-4">
                   <NavItem className='w-100' onClick={()=> setManageProfileSwitch("basic")} >
                     <NavLink className='border border-top-primary'>
-                      Basic Information 
+                      Basic Information .
                     </NavLink>
                   </NavItem>
                   <NavItem className='w-100' onClick={()=> setManageProfileSwitch("education")} >
@@ -116,11 +96,6 @@ const General = () => {
                       Education Information
                     </NavLink>
                   </NavItem>
-                  {/* <NavItem className='w-100' onClick={()=> setManageProfileSwitch("family")} >
-                    <NavLink className='border border-top-primary'>
-                      Family Information
-                    </NavLink>
-                  </NavItem> */}
                 </Nav>
               </Col>
               <Col sm="8" xs="8" md="8" lg="8" xl="8" id="tab-type2" className='ml-0'>
