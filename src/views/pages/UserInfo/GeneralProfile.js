0efe8eaf54a34ac093456359335397525461d395
Patrick <pmarks1914@gmail.com>
2024-07-26 00:03:15 +0000

    tab manage fix

--- src/views/pages/UserInfo/GeneralProfile.js
+++ src/views/pages/UserInfo/GeneralProfile.js
@@ -74,17 +74,9 @@ const General = () => {
         <NavItem>
           <NavLink
             className={classnames({ active: activeTab === '2' })}
-            onClick={() => { toggle('2'); }}
+            onClick={() => { toggle('2'); setManageProfileSwitch("education") }}
           >
             <h6>Education Information</h6>
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '1' })}
-            onClick={() => { toggle('1'); }}
-          >
-            <h6>Profile</h6>
           </NavLink>
         </NavItem>
         <br /><br />
@@ -108,6 +100,29 @@ const General = () => {
               </Col>
               <Col sm="8" xs="8" md="8" lg="8" xl="8" id="tab-type2" className='ml-0'>
                 <BasicInfo profileManage={manageProfileSwitch} />
+              </Col>
+              <Col sm="0" xs="0" md="2" lg="2" xl="2" className='ml-0'>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="1">
+            <Row>
+              <Col sm="4" xs="4" md="2" lg="2" xl="2" className='mt-5 mr-0 bg-white'>
+                <Nav className="mt-4">
+                  <NavItem className='w-100' onClick={()=> setManageProfileSwitch("basic")} >
+                    <NavLink className='border border-top-primary'>
+                      Basic Information .
+                    </NavLink>
+                  </NavItem>
+                  <NavItem className='w-100' onClick={()=> setManageProfileSwitch("education")} >
+                    <NavLink className='border border-top-primary'>
+                      Education Information
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+              </Col>
+              <Col sm="8" xs="8" md="8" lg="8" xl="8" id="tab-type2" className='ml-0'>
+                <BasicInfo profileManage={"education"} />
               </Col>
               <Col sm="0" xs="0" md="2" lg="2" xl="2" className='ml-0'>
               </Col>
