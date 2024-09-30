71347d48b8176d96da69fb1eb286628f3034e78c
Patrick <pmarks1914@gmail.com>
2024-09-30 23:14:04 +0000

    clean up

--- src/views/pages/Evaluation/EvaluationDetail.js
+++ src/views/pages/Evaluation/EvaluationDetail.js
@@ -109,9 +109,7 @@ const EvaluationDetail = () => {
     }
     return (
         <div className='d-flex justify-content-center' style={{ margin: '0px 0px 0px 0px' }}  >
-            {/* <Counter /> */}
             <Box style={{ width: '70%', margin: '0px 0px' }} >
-                {/* <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}> */}
 
                 {
                     evaluationData?.evaluation_info ?
